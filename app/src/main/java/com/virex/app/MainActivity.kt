6afd23dc54ac5321abf0275925ae959a637c6d73
Virex Developer <dev@virex.app>
2026-02-24 07:47:33 +0500

    Remove Hilt dependency to fix ANR/hang issue - app now responsive

--- app/src/main/java/com/virex/app/MainActivity.kt
+++ app/src/main/java/com/virex/app/MainActivity.kt
@@ -9,16 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
